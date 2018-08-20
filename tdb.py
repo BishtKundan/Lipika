c9b24803d0615f1832143dad5beafcb13dd075c1
Kundan Bisht <kundanb@hotmail.com>
2018-08-20 18:16:07 +0530

    changes to TDB

--- tdb.py
+++ tdb.py
@@ -1,3 +1,4 @@
+import cgi, cgitb
 import pyodbc 
 cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                       "Server=BES01L4KBISHT\SQLEXPRESS;"
@@ -7,5 +8,13 @@ cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
 cursor = cnxn.cursor()
 cursor.execute('select firstname,lastname from employee')
 
-for row in cursor:
-    print('row = %r' % (row,))
+print('Content-Type: text/plain')
+print('')
+#print('Hello, world!')
+
+for row in cursor.fetchall():
+	print(row)
+
+cursor.execute('select firstname,lastname from employee')	
+for row in cursor.fetchall():
+	print(row[0])
